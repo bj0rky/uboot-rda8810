e905e6953c189c61974703623cdc12941bb7bb01
Bot Fap <botfap@gmail.com>
2017-11-08 22:29:04 +0000

    Change default boot command to fatload not ext2load

--- include/configs/rda_config_defaults.h
+++ include/configs/rda_config_defaults.h
@@ -285,7 +285,7 @@
 #define CONFIG_BOOTCOMMAND		\
 	"mux_config; "		\
 	"mmc dev 0; "		\
-	"ext2load mmc 0:1 ${script_addr} boot.scr && source ${script_addr};" \
+	"fatload mmc 0:1 ${script_addr} boot.scr && source ${script_addr};" \
 	"echo Running boot script failed;"
 #endif /* CONFIG_RDA_PDL */
 
